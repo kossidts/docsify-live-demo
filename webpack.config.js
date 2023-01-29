cbab186c065803f8a609e37a44f76aa42cfc3832
Kossi D. T. Saka <kdtsaka@gmail.com>
2023-01-29 19:00:13 +0100

    Improve the build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 import path from "node:path";
 import { exec } from "node:child_process";
-// import fs from "node:fs";
+import fs from "node:fs";
 
-// import webpack from "webpack";
+import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import TerserPlugin from "terser-webpack-plugin";
 
 // const currentTask = process.env.npm_lifecycle_event;
 const distPath = path.join(process.cwd(), "dist");
 const exampleAssetsPath = path.join(process.cwd(), "example", "docs", "assets");
 const currentTask = process.env.npm_lifecycle_event;
+
+const pkg = JSON.parse(fs.readFileSync(path.join(process.cwd(), "./package.json"), { encoding: "utf8" }));
 
 class DeployPlugin {
     constructor(options) {
@@ -36,7 +39,7 @@ const config = {
         path: path.join(process.cwd(), "dist"),
         filename: "[name].js",
     },
-    mode: "production",
+    mode: "development",
     module: {
         rules: [
             {
@@ -56,7 +59,26 @@ const config = {
         ],
     },
     plugins: [new MiniCssExtractPlugin({ filename: "[name].css" }), new DeployPlugin()],
-    watch: currentTask !== "build",
+    watch: true,
 };
+
+if (currentTask == "build") {
+    config.mode = "production";
+    config.watch = false;
+    config.plugins.push(
+        new webpack.BannerPlugin({
+            entryOnly: true,
+            banner: `${pkg.name} v${pkg.version} by ${pkg.author} | ${pkg.license}`,
+        })
+    );
+    config.optimization = {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
+    };
+}
 
 export default config;
